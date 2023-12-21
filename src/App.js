cece8f8e51e19dcdc4f75ce862d22f765d9ba413
Lindsey Ipson <lindseyipson@Lindseys-MacBook-Air.local>
2023-12-20 22:01:09 -0800

    added getDrinks method to SnackOrBoozeAPI class  and connect drinks to Home component

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import SnackOrBoozeApi from "./Api";
+import NavBar from "./NavBar";
+import { Route, Switch } from "react-router-dom";
+import Menu from "./FoodMenu";
+import Snack from "./FoodItem";
+
+function App() {
+  const [snacksIsLoading, setSnacksIsLoading] = useState(true);
+  const [snacks, setSnacks] = useState([]);
+
+  useEffect(() => {
+    async function getSnacks() {
+      let snacks = await SnackOrBoozeApi.getSnacks();
+      setSnacks(snacks);
+      setSnacksIsLoading(false);
+    }
+    getSnacks();
+  }, []);
+
+  const [drinksIsLoading, setDrinksIsLoading] = useState(true);
+  const [drinks, setDrinks] = useState([]);
+
+  useEffect(() => {
+    async function getDrinks() {
+      let drinks = await SnackOrBoozeApi.getDrinks();
+      setDrinks(drinks);
+      setDrinksIsLoading(false);
+    }
+    getDrinks();
+  }, []);
+
+
+
+  if (snacksIsLoading || snacksIsLoading) {
+    return <p>Loading &hellip;</p>;
+  }
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <Home snacks={snacks} drinks={drinks} />
+            </Route>
+            <Route exact path="/snacks">
+              <Menu snacks={snacks} title="Snacks" />
+            </Route>
+            <Route path="/snacks/:id">
+              <Snack items={snacks} cantFind="/snacks" />
+            </Route>
+            <Route>
+              <p>Hmmm. I can't seem to find what you want.</p>
+            </Route>
+          </Switch>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
